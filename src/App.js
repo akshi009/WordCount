328ef5e3450cea45ce15ff48b84a669df7268c33
akshi009 <akshi20jain03@gamil.com>
2024-03-14 13:04:04 +0530

    wordcount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
+  const [myStyle, setMyStyle] = useState({
+    backgroundColor: "#1E1E1E",
+    color: "white",
+  });
+
+  const black = () => {
+    if (
+      myStyle.backgroundColor === "white" ||
+      myStyle.backgroundColor === "#e0f0e3"
+    ) {
+      setMyStyle({
+        backgroundColor: "#1E1E1E",
+        color: "white",
+      });
+      document.title = "Wordcount-DarkMode";
+    }
+  };
+
+  const green = () => {
+    if (
+      myStyle.backgroundColor === "white" ||
+      myStyle.backgroundColor === "#1E1E1E"
+    ) {
+      setMyStyle({
+        backgroundColor: "#e0f0e3",
+        color: "#1E1E1E",
+      });
+    }
+  };
+
+  const white = () => {
+    if (
+      myStyle.backgroundColor === "#e0f0e3" ||
+      myStyle.backgroundColor === "#1E1E1E"
+    ) {
+      setMyStyle({
+        backgroundColor: "white",
+        color: "#1E1E1E",
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="h-screen" style={myStyle}>
+        <Navbar title="YOYO" green={green} black={black} white={white} />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<TextForm />} />
+        </Routes>
+        {/* <div className="container my-5">
+          <TextForm />
+        </div> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar(props) {
+
+ 
+  
+  return (
+   
+    <nav className={`navbar navbar-expand-lg bg-gray-800 text-white flex`}>
+       
+  <div className={`container-fluid flex gap-5 w-full `}>
+    <Link className="navbar-brand m-2 text-white" to="/">{props.title}</Link>
+    
+    <div>
+    </div>
+    <div className="collapse flex  w-full m-2 justify-between navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav flex gap-3 me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-white" to="/about">About</Link>
+        </li>
+        
+       
+      </ul>
+      
+ 
+  <div onClick={props.black} className="bg-blue-500 right-2 m-2 absolute cursor-pointer hover:bg-blue-600 rounded-sm text-white p-2 mx-2">Black Mode</div>
+  <div onClick={props.green} className="bg-blue-500 right-28  m-2 absolute cursor-pointer hover:bg-blue-600 rounded-sm text-white p-2 mx-2 mb-2">Green Mode</div>
+  <div onClick={props.white} className="bg-blue-500 right-56  m-2 absolute cursor-pointer hover:bg-blue-600 rounded-sm text-white p-2 mx-2 mb-2">Light Mode</div>
+</div>
+
+    </div>
+   
+  
+</nav>
+
+  )
+}
